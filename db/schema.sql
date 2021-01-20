9c3d1a0cb5e18672b21cba8b0e1eb636e43959b6
Clu <retsulcconnor@gmail.com>
2021-01-19 23:17:47 -0500

    populated and joined tables with dummy data

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,19 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL NOT NULL,
     department_id INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT UNSIGNED NOT NULL,
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const inquirer = require('inquirer');
 // const db = require('./db/databse');
 const mysql = require('mysql2');
+// const Employees = require('./lib/employee');
+
+// Connection to mySQL server for Inquirer to call upon
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
-    database: 'test'
+    password: 'password',
+    database: 'company_db'
 });
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+  });
 
-connection.query(
-    `SELECT * FROM employee WHERE role_id = 1`,
-    ['Page', 45],
-    function(err, results) {
-        console.log(results);
-    }
-);
+afterConnection = () => {
+    connection.query(
+        `SELECT * FROM employee LEFT JOIN (role, department)
+        ON (role.id = employee.role_id AND department.id = role.department_id)`,
+        function(err, result) {
+            console.table(result);
+        }
+    );
+    connection.end();
+};
+
