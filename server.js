c42f92ee4165fd39de313c64f1eb0eeb0ade0e46
Clu <retsulcconnor@gmail.com>
2021-01-20 23:03:31 -0500

    populated all Employees data table from Inquirer

--- server.js
+++ server.js
@@ -6,21 +6,78 @@ const mysql = require('mysql2');
 // Connection to mySQL server for Inquirer to call upon
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    // port: 3306,
     user: 'root',
     password: 'password',
     database: 'company_db'
 });
-connection.connect(err => {
-    if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-    afterConnection();
-  });
 
-afterConnection = () => {
+function promptUser() {
+    inquirer
+        .prompt ({
+        type: 'list',
+        name: 'actionsMenu',
+        message: 'What would you like to do?',
+        choices: ['View all employees', 'View all departments', 'View all roles', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee']
+        })
+        .then(choices => {
+            switch (choices['actionsMenu']) {
+                case 'View all employees':
+                    allEmployees();
+                    // connection.connect(err => {
+                    //     if (err) throw err;
+                    //     console.log('connected as id ' + connection.threadId);
+                    //     afterConnection();
+                    // });
+                    // afterConnection = () => {
+                    //     connection.query(
+                    //         `SELECT * FROM employee`,
+                    //         function(err, result) {
+                    //             console.table(result);
+                    //         }
+                    //     );
+                    //     connection.end();
+                    // };
+                    break;
+                case 'View all departments':
+                    connection.connect(err => {
+                        if (err) throw err;
+                        console.log('connected as id ' + connection.threadId);
+                        afterConnection();
+                    });
+                    afterConnection = () => {
+                        connection.query(
+                            `SELECT * FROM department`,
+                            function(err, result) {
+                                console.table(result);
+                            }
+                        );
+                        connection.end();
+                    };
+                    break;
+                case 'View all departments':
+                    connection.connect(err => {
+                        if (err) throw err;
+                        console.log('connected as id ' + connection.threadId);
+                        afterConnection();
+                    });
+                    afterConnection = () => {
+                        connection.query(
+                            `SELECT * FROM role`,
+                            function(err, result) {
+                                console.table(result);
+                            }
+                        );
+                        connection.end();
+                    };
+                    break;
+            }
+        })
+} 
+
+const allEmployees = () => {
     connection.query(
-        `SELECT * FROM employee LEFT JOIN (role, department)
-        ON (role.id = employee.role_id AND department.id = role.department_id)`,
+        `SELECT * FROM employee`,
         function(err, result) {
             console.table(result);
         }
@@ -28,3 +85,56 @@ afterConnection = () => {
     connection.end();
 };
 
+// const initialQuery = () => {
+//     inquirer
+//         .prompt(initialPrompt)
+            
+                // if (choices === 'View all employees') {
+                //     connection.connect(err => {
+                //         if (err) throw err;
+                //         console.log('connected as id ' + connection.threadId);
+                //         afterConnection();
+                //     });
+                //     afterConnection = () => {
+                //         connection.query(
+                //             `SELECT * FROM employee`,
+                //             function(err, result) {
+                //                 console.table(result);
+                //             }
+                //         );
+                //         connection.end();
+                //     };
+                // } else if (choices === 'View all departments') {
+                //     connection.connect(err => {
+                //         if (err) throw err;
+                //         console.log('connected as id ' + connection.threadId);
+                //         afterConnection();
+                //     });
+                //     afterConnection = () => {
+                //         connection.query(
+                //             `SELECT * FROM department`,
+                //             function(err, result) {
+                //                 console.table(result);
+                //             }
+                //         );
+                //         connection.end();
+                //     };
+                // } else if (choices === 'View all roles') {
+                //     connection.connect(err => {
+                //         if (err) throw err;
+                //         console.log('connected as id ' + connection.threadId);
+                //         afterConnection();
+                //     });
+                //     afterConnection = () => {
+                //         connection.query(
+                //             `SELECT * FROM role`,
+                //             function(err, result) {
+                //                 console.table(result);
+                //             }
+                //         );
+                //         connection.end();
+                //     }
+                // };
+
+
+promptUser();
